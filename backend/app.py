c9852bf82abee19eff31a1e2cf72c03c7539b459
Lakindu Kariyawasam <lckariyawasam@yahoo.com>
2023-09-20 19:59:52 +0530

    Add login route, not functional

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        return "Login Successful"
+    
+    return "Page Under Construction"
