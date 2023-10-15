5852096320c671c0517d4657c28cedc5942fe801
Roshane Moraes <125941940+roshanemoraes@users.noreply.github.com>
2023-10-15 19:03:16 +0530

    Create product.sql

---
+++ Database/Dummy data per table/product.sql
@@ -0,0 +1,99 @@
+ -- Insert data for Mobile Phones
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('iPhone 12', '150g', 'Apple', 'The iPhone 12 is a flagship smartphone from Apple, featuring a stunning Super Retina XDR display, A14 Bionic chip, and a dual-camera system. It comes in various storage capacities and colors, providing a premium user experience.', 'Storage', 'Color'),
+  ('Samsung Galaxy S21', '170g', 'Samsung', 'The Samsung Galaxy S21 is a powerful Android phone with a brilliant AMOLED display, Exynos 2100 processor, and excellent camera capabilities. It comes in different storage options and attractive color choices.', 'Storage', 'Color'),
+  ('Google Pixel 5', '151g', 'Google', 'The Google Pixel 5 is a pure Android experience with a fantastic camera, 5G support, and a sleek design. It comes with multiple storage options and eye-catching color variants.', 'Storage', 'Color'),
+  ('OnePlus 9', '170g', 'OnePlus', 'The OnePlus 9 is a flagship killer with a high-refresh-rate display, Snapdragon 888 chipset, and Hasselblad-tuned cameras. It offers ample storage and color selections for tech enthusiasts.', 'Storage', 'Color'),
+  ('Xiaomi Mi 11', '196g', 'Xiaomi', 'The Xiaomi Mi 11 is a budget-friendly flagship with a vibrant display, Snapdragon 888 processor, and versatile camera system. It''s available in various storage and color combinations.', 'Storage', 'Color');
+
+-- Insert data for TVs
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Samsung 55" 4K Smart TV', '20kg', 'Samsung', 'The Samsung 55" 4K Smart TV is a premium television with a large and vibrant screen, offering 4K resolution for a stunning visual experience. Its perfect for enjoying your favorite movies, shows, and games.', 'Screen Size', 'Resolution'),
+  ('LG 65" OLED TV', '25kg', 'LG', 'The LG 65" OLED TV provides an exceptional viewing experience with its OLED display technology, offering deep blacks and vibrant colors. With a large screen, its ideal for cinematic entertainment.', 'Screen Size', 'Resolution'),
+  ('Sony 60" 4K Smart TV', '22kg', 'Sony', 'The Sony 60" 4K Smart TV is designed to deliver exceptional picture quality with 4K resolution. Its a smart TV that offers a range of features for streaming and connectivity, making it a great addition to any home theater.', 'Screen Size', 'Resolution'),
+  ('TCL 50" LED TV', '18kg', 'TCL', 'The TCL 50" LED TV is a reliable and affordable choice for those seeking a quality television with a 50-inch screen. It offers clear and crisp images with a focus on energy efficiency.', 'Screen Size', 'Resolution'),
+  ('Hisense 43" 1080p TV', '15kg', 'Hisense', 'The Hisense 43" 1080p TV is a compact and budget-friendly option, ideal for smaller spaces. It delivers Full HD resolution and is perfect for casual viewing and gaming.', 'Screen Size', 'Resolution');
+
+-- Insert data for Laptops
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Dell XPS 13', '1.2kg', 'Dell', 'The Dell XPS 13 is a lightweight and powerful laptop. It offers excellent portability and features a variety of storage and RAM options, making it a versatile choice for work and entertainment.', 'Storage Size', 'RAM Size'),
+  ('MacBook Air', '1.3kg', 'Apple', 'The MacBook Air is a sleek and elegant laptop by Apple. Known for its thin and lightweight design, its a great choice for on-the-go users. Its available with different storage and RAM configurations.', 'Storage Size', 'RAM Size'),
+  ('HP Spectre x360', '1.4kg', 'HP', 'The HP Spectre x360 is a versatile 2-in-1 laptop with a premium build. Its designed for flexibility and offers various storage and RAM options, ensuring you can find the perfect fit for your needs.', 'Storage Size', 'RAM Size'),
+  ('Lenovo ThinkPad X1 Carbon', '1.35kg', 'Lenovo', 'The Lenovo ThinkPad X1 Carbon is a business-class laptop known for its durability and performance. With different storage and RAM sizes available, its an excellent choice for professionals.', 'Storage Size', 'RAM Size'),
+  ('Asus ROG Zephyrus G14', '1.7kg', 'Asus', 'The Asus ROG Zephyrus G14 is a gaming laptop designed for high-performance gaming. It offers various storage and RAM configurations to meet the demands of gamers and enthusiasts.', 'Storage Size', 'RAM Size');
+
+
+-- Insert data for Headphones
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Sony WH-1000XM4', '250g', 'Sony', 'The Sony WH-1000XM4 headphones offer an exceptional audio experience. With advanced noise-canceling technology and a variety of stylish colors, they provide an immersive and personalized listening journey.', 'Color', 'Noise Cancelling'),
+  ('Bose QuietComfort 35 II', '235g', 'Bose', 'The Bose QuietComfort 35 II headphones are renowned for their noise-canceling prowess. They come in different colors and deliver a premium audio experience for those seeking tranquility.', 'Color', 'Noise Cancelling'),
+  ('Sennheiser HD 660 S', '260g', 'Sennheiser', 'The Sennheiser HD 660 S headphones are designed for audiophiles. With various color options and advanced noise-canceling, they ensure a superb and immersive listening adventure.', 'Color', 'Noise Cancelling'),
+  ('Beats Solo Pro', '220g', 'Beats', 'The Beats Solo Pro headphones offer a stylish audio solution. Equipped with noise-canceling and available in different colors, they provide a premium auditory experience for fashion-conscious users.', 'Color', 'Noise Cancelling'),
+  ('JBL Free X', '95g', 'JBL', 'The JBL Free X headphones provide wireless convenience and come in various attractive colors. They offer a lightweight and cordless audio experience for those on the move.', 'Color', 'Wireless');
+
+-- Insert data for Cameras
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Canon EOS 5D Mark IV', '800g', 'Canon', 'The Canon EOS 5D Mark IV is a professional DSLR camera known for its high-resolution images. It features advanced megapixels and a versatile lens system, making it a top choice for photographers and videographers.', 'Megapixels', 'Lens Type'),
+  ('Nikon D750', '750g', 'Nikon', 'The Nikon D750 is a powerful DSLR camera designed for enthusiasts and professionals. With impressive megapixels and a range of lens options, it provides superb image quality and versatility.', 'Megapixels', 'Lens Type'),
+  ('Sony Alpha A7 III', '650g', 'Sony', 'The Sony Alpha A7 III is a mirrorless camera renowned for its outstanding image quality. With cutting-edge megapixels and a variety of lens choices, it delivers exceptional results for photographers and content creators.', 'Megapixels', 'Lens Type'),
+  ('Panasonic Lumix GH5', '725g', 'Panasonic', 'The Panasonic Lumix GH5 is a versatile mirrorless camera tailored for both photography and videography. Offering impressive megapixels and lens diversity, its a go-to choice for creative professionals.', 'Megapixels', 'Lens Type'),
+  ('Fujifilm X-T4', '580g', 'Fujifilm', 'The Fujifilm X-T4 is a mirrorless camera perfect for photography and videography. With advanced megapixels and various lens options, its designed to inspire creativity and capture stunning visuals.', 'Megapixels', 'Lens Type');
+
+-- Insert data for Watches
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Apple Watch Series 6', '45g', 'Apple', 'The Apple Watch Series 6 is a premium smartwatch that offers an array of health and fitness features. Available in various sizes and stylish colors, it seamlessly integrates with your Apple devices.', 'Size', 'Color'),
+  ('Samsung Galaxy Watch 3', '52g', 'Samsung', 'The Samsung Galaxy Watch 3 is a feature-packed smartwatch with a strong focus on health tracking. It comes in different sizes and attractive color options, making it a perfect companion for Samsung users.', 'Size', 'Color'),
+  ('Garmin Forerunner 945', '50g', 'Garmin', 'The Garmin Forerunner 945 is a high-performance GPS running watch designed for athletes. Its available in different sizes and stylish colors, making it a great tool for tracking and improving your workouts.', 'Size', 'Color'),
+  ('Fossil Gen 5', '55g', 'Fossil', 'The Fossil Gen 5 is a fashionable smartwatch that combines style with functionality. With multiple size options and eye-catching color choices, its a great accessory for tech-savvy individuals.', 'Size', 'Color'),
+  ('Fitbit Versa 3', '40g', 'Fitbit', 'The Fitbit Versa 3 is a fitness-focused smartwatch that excels at tracking your health and workouts. It comes in various sizes and appealing colors, helping you achieve your fitness goals in style.', 'Size', 'Color');
+
+-- Insert data for Home Appliances
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Samsung Refrigerator', '85kg', 'Samsung', 'The Samsung Refrigerator is a high-capacity and energy-efficient appliance that keeps your food fresh. With different capacities and color choices, it complements your kitchen and storage needs.', 'Capacity', 'Color'),
+  ('LG Washing Machine', '65kg', 'LG', 'The LG Washing Machine is a reliable laundry appliance with various load capacities and color options. Its designed to make your laundry tasks easier and more efficient.', 'Capacity', 'Color'),
+  ('Whirlpool Microwave Oven', '30kg', 'Whirlpool', 'The Whirlpool Microwave Oven is a versatile kitchen appliance that offers different cooking capacities and color selections. It simplifies your cooking and heating needs.', 'Capacity', 'Color'),
+  ('Breville Coffee Maker', '5kg', 'Breville', 'The Breville Coffee Maker is a stylish and functional coffee machine with varying capacities and color variants. It brews your favorite coffee just the way you like it.', 'Capacity', 'Color'),
+  ('Dyson Vacuum Cleaner', '7kg', 'Dyson', 'The Dyson Vacuum Cleaner is a powerful cleaning solution for your home. It comes in different capacities and colors, making cleaning tasks a breeze with a touch of style.', 'Capacity', 'Color');
+
+-- Insert data for Gaming Consoles
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('PlayStation 5', '4.5kg', 'Sony', 'The PlayStation 5 is a next-gen gaming console that offers an immersive gaming experience. It comes in various storage options and color variations, providing top-tier gaming entertainment.', 'Storage', 'Color'),
+  ('Xbox Series X', '4.4kg', 'Microsoft', 'The Xbox Series X is a powerful gaming console designed for high-performance gaming. Its available in different storage capacities and attractive color choices, delivering top-quality gaming experiences.', 'Storage', 'Color'),
+  ('Nintendo Switch', '0.88kg', 'Nintendo', 'The Nintendo Switch is a versatile gaming system that offers on-the-go gaming fun. Its available with various storage options and stylish color selections, providing unique gaming adventures.', 'Storage', 'Color'),
+  ('Sony PlayStation 4', '2.8kg', 'Sony', 'The Sony PlayStation 4 is a popular gaming console known for its extensive game library. It comes with different storage capacities and color options, offering diverse gaming experiences.', 'Storage', 'Color'),
+  ('Xbox One X', '4.43kg', 'Microsoft', 'The Xbox One X is a gaming console that delivers 4K gaming and entertainment. Its available in multiple storage options and sleek color variants, offering a fantastic gaming and multimedia experience.', 'Storage', 'Color');
+
+-- Insert data for Smart Home Security
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Ring Alarm Security Kit', '1.1kg', 'Ring', 'The Ring Alarm Security Kit is a comprehensive home security solution. It includes various components for protecting your home and comes in a range of colors to match your style and decor.', 'Components', 'Color'),
+  ('Arlo Pro 4 Security Camera', '400g', 'Arlo', 'The Arlo Pro 4 Security Camera is a high-resolution wireless camera for monitoring your property. It offers different color options and ensures you have a clear view of your surroundings.', 'Resolution', 'Color'),
+  ('Nest Hello Video Doorbell', '180g', 'Nest', 'The Nest Hello Video Doorbell enhances your homes security and convenience. Its compatible with your smart home system and offers stylish color choices to suit your entryway.', 'Compatibility', 'Color'),
+  ('SimpliSafe Home Security System', '2.2kg', 'SimpliSafe', 'The SimpliSafe Home Security System is a reliable solution for protecting your home. It includes various components to ensure safety and security. You can choose from a range of colors to match your decor.', 'Components', 'Color'),
+  ('Blink Mini Indoor Camera', '65g', 'Blink', 'The Blink Mini Indoor Camera is a compact security camera for monitoring the inside of your home. It offers different resolution options and is available in various colors to blend seamlessly with your interior.', 'Resolution', 'Color');
+
+-- Insert data for Printers
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('HP LaserJet Pro', '11kg', 'HP', 'The HP LaserJet Pro is a reliable and efficient printer for your business needs. It offers high print speed and vibrant color printing. Perfect for offices and professional use.', 'Print Speed', 'Color'),
+  ('Epson EcoTank', '6.8kg', 'Epson', 'The Epson EcoTank printer is an eco-friendly choice with cost-saving ink tanks. It delivers fast print speeds and stunning color prints, making it an ideal solution for home and small office printing.', 'Print Speed', 'Color'),
+  ('Canon PIXMA', '5.5kg', 'Canon', 'The Canon PIXMA printer is known for its exceptional print quality and versatility. It boasts high print speed and vivid color output, making it a great choice for both documents and photos.', 'Print Speed', 'Color'),
+  ('Brother HL-L2350DW', '7kg', 'Brother', 'The Brother HL-L2350DW is a compact monochrome laser printer designed for efficiency. It offers fast print speeds and high-quality text output, perfect for business and home use.', 'Print Speed', 'Color'),
+  ('Xerox WorkCentre', '15kg', 'Xerox', 'The Xerox WorkCentre is a heavy-duty multifunction printer with impressive print speed and color capabilities. Its a reliable choice for businesses seeking high-performance printing and copying.', 'Print Speed', 'Color');
+
+-- Insert data for Tablets
+INSERT INTO Ecom_platform.Product (title, weight, brand, description, Variant_attribute_type_1, Variant_attribute_type_2)
+VALUES
+  ('Apple iPad Pro', '470g', 'Apple', 'The Apple iPad Pro is a top-tier tablet with a powerful A12Z Bionic chip, stunning Retina display, and support for the Apple Pencil. Available in various storage options and colors for premium performance and style.', 'Storage', 'Color'),
+  ('Samsung Galaxy Tab S7', '500g', 'Samsung', 'The Samsung Galaxy Tab S7 is a versatile Android tablet featuring a Super AMOLED display, Snapdragon 865+ processor, and S Pen support. It comes in different storage capacities and attractive color choices for productivity and entertainment.', 'Storage', 'Color'),
+  ('Microsoft Surface Pro 7', '770g', 'Microsoft', 'The Microsoft Surface Pro 7 is a 2-in-1 tablet with laptop-like performance. It boasts an Intel Core processor, PixelSense display, and versatility for work and play. Its available in various storage options and stylish colors.', 'Storage', 'Color'),
+  ('Amazon Fire HD 10', '500g', 'Amazon', 'The Amazon Fire HD 10 is a budget-friendly tablet for entertainment and productivity. It offers a Full HD display, long battery life, and access to Amazons content ecosystem. Available in different storage capacities and colors.', 'Storage', 'Color'),
+  ('Lenovo Tab M10', '480g', 'Lenovo', 'The Lenovo Tab M10 is an affordable Android tablet with a family-friendly design. It features a bright display, Dolby Atmos audio, and ample storage options in a sleek form. Choose from a variety of storage capacities and appealing colors.', 'Storage', 'Color');
