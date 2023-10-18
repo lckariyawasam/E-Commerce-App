298f30c7e3d2855a7f3c4aed154776109fbd4dae
Roshane Moraes <125941940+roshanemoraes@users.noreply.github.com>
2023-10-18 11:27:44 +0530

    Update dummy_data_v1.4.sql

--- Database/dummy_data_v1.4.sql
+++ Database/dummy_data_v1.4.sql
@@ -657,171 +657,171 @@ VALUES
   (45, 'Pending');
   
 -- Example Cart_item records
-INSERT INTO Ecom_platform.Cart_item (variant_id, cart_id, quantity, status, sold_date)
+INSERT INTO Ecom_platform.Cart_item (variant_id, cart_id, quantity, status, sold_date, sold_price_per_item)
 VALUES
 -- Cart 1
-  (1, 1, 2, 'Pending', NULL), -- Mobile Phones (variant_id 1) - 2 items
-  (11, 1, 1, 'Pending', NULL), -- TVs (variant_id 11) - 1 item
-  (51, 1, 2, 'Pending', NULL), -- Watches (variant_id 51) - 2 items
-  (61, 1, 4, 'Pending', NULL), -- Home Appliances (variant_id 61) - 4 items
+  (1, 1, 2, 'Pending', NULL, NULL), -- Mobile Phones (variant_id 1) - 2 items
+  (11, 1, 1, 'Pending', NULL, NULL), -- TVs (variant_id 11) - 1 item
+  (51, 1, 2, 'Pending', NULL, NULL), -- Watches (variant_id 51) - 2 items
+  (61, 1, 4, 'Pending', NULL, NULL), -- Home Appliances (variant_id 61) - 4 items
 
 -- Cart 2
-  (1, 2, 3, 'Pending', NULL), -- Mobile Phones (variant_id 1) - 3 items
-  (12, 2, 2, 'Pending', NULL), -- TVs (variant_id 12) - 2 items
-  (22, 2, 1, 'Pending', NULL), -- Laptops (variant_id 22) - 1 item
-  (32, 2, 4, 'Pending', NULL), -- Headphones (variant_id 32) - 4 items
-  (42, 2, 3, 'Pending', NULL), -- Cameras (variant_id 42) - 3 items
+  (1, 2, 3, 'Pending', NULL, NULL), -- Mobile Phones (variant_id 1) - 3 items
+  (12, 2, 2, 'Pending', NULL, NULL), -- TVs (variant_id 12) - 2 items
+  (22, 2, 1, 'Pending', NULL, NULL), -- Laptops (variant_id 22) - 1 item
+  (32, 2, 4, 'Pending', NULL, NULL), -- Headphones (variant_id 32) - 4 items
+  (42, 2, 3, 'Pending', NULL, NULL), -- Cameras (variant_id 42) - 3 items
 
 -- Cart 3
-  (13, 3, 2, 'Complete', '2023-10-12'), -- TVs (variant_id 13) - 2 items (sold)
-  (23, 3, 1, 'Complete', '2023-10-12'), -- Laptops (variant_id 23) - 1 item (sold)
-  (53, 3, 2, 'Complete', '2023-10-12'), -- Watches (variant_id 53) - 2 items (sold)
-  (63, 3, 1, 'Complete', '2023-10-12'), -- Home Appliances (variant_id 63) - 1 item (sold)
+  (13, 3, 2, 'Complete', '2023-10-12','1299.99'), -- TVs (variant_id 13) - 2 items (sold)
+  (23, 3, 1, 'Complete', '2023-10-12','749.99'), -- Laptops (variant_id 23) - 1 item (sold)
+  (53, 3, 2, 'Complete', '2023-10-12','269.99'), -- Watches (variant_id 53) - 2 items (sold)
+  (63, 3, 1, 'Complete', '2023-10-12','349.99'), -- Home Appliances (variant_id 63) - 1 item (sold)
 
 -- Cart 4
-  (24, 4, 2, 'Pending', NULL), -- Laptops (variant_id 24) - 2 items
-  (34, 4, 6, 'Pending', NULL), -- Headphones (variant_id 34) - 6 items
-  (44, 4, 5, 'Pending', NULL), -- Cameras (variant_id 44) - 5 items
-  (54, 4, 3, 'Pending', NULL), -- Watches (variant_id 54) - 3 items
-  (64, 4, 2, 'Pending', NULL), -- Home Appliances (variant_id 64) - 2 items
+  (24, 4, 2, 'Pending', NULL, NULL), -- Laptops (variant_id 24) - 2 items
+  (34, 4, 6, 'Pending', NULL, NULL), -- Headphones (variant_id 34) - 6 items
+  (44, 4, 5, 'Pending', NULL, NULL), -- Cameras (variant_id 44) - 5 items
+  (54, 4, 3, 'Pending', NULL, NULL), -- Watches (variant_id 54) - 3 items
+  (64, 4, 2, 'Pending', NULL, NULL), -- Home Appliances (variant_id 64) - 2 items
   
 -- Cart 5
-  (5, 5, 2, 'Complete', '2023-10-13'), -- Mobile Phones (variant_id 5) - 2 items (sold)
-  (15, 5, 1, 'Complete', '2023-10-13'), -- TVs (variant_id 15) - 1 item (sold)
+  (5, 5, 2, 'Complete', '2023-10-13', '599.99'), -- Mobile Phones (variant_id 5) - 2 items (sold)
+  (15, 5, 1, 'Complete', '2023-10-13', '849.99'), -- TVs (variant_id 15) - 1 item (sold)
 
 -- Cart 6
-  (6, 6, 2, 'Complete', '2023-10-14'), -- Mobile Phones (variant_id 6) - 2 items (sold)
-  (16, 6, 1, 'Complete', '2023-10-14'), -- TVs (variant_id 16) - 1 item (sold)
-  (26, 6, 2, 'Complete', '2023-10-14'), -- Laptops (variant_id 26) - 2 items (sold)
-  (36, 6, 1, 'Complete', '2023-10-14'), -- Headphones (variant_id 36) - 1 item (sold)
-  (46, 6, 2, 'Complete', '2023-10-14'), -- Cameras (variant_id 46) - 2 items (sold)
+  (6, 6, 2, 'Complete', '2023-10-14', '549.99'), -- Mobile Phones (variant_id 6) - 2 items (sold)
+  (16, 6, 1, 'Complete', '2023-10-14', '699.99'), -- TVs (variant_id 16) - 1 item (sold)
+  (26, 6, 2, 'Complete', '2023-10-14', '899.99'), -- Laptops (variant_id 26) - 2 items (sold)
+  (36, 6, 1, 'Complete', '2023-10-14', '179.99'), -- Headphones (variant_id 36) - 1 item (sold)
+  (46, 6, 2, 'Complete', '2023-10-14', '699.99'), -- Cameras (variant_id 46) - 2 items (sold)
 
 -- Cart 7
-  (7, 7, 3, 'Pending', NULL), -- Mobile Phones (variant_id 7) - 3 items
-  (17, 7, 2, 'Pending', NULL), -- TVs (variant_id 17) - 2 items
+  (7, 7, 3, 'Pending', NULL, NULL), -- Mobile Phones (variant_id 7) - 3 items
+  (17, 7, 2, 'Pending', NULL, NULL), -- TVs (variant_id 17) - 2 items
 
 -- Cart 8
-  (8, 8, 4, 'Pending', NULL), -- Mobile Phones (variant_id 8) - 4 items
-  (18, 8, 3, 'Pending', NULL), -- TVs (variant_id 18) - 3 items
+  (8, 8, 4, 'Pending', NULL, NULL), -- Mobile Phones (variant_id 8) - 4 items
+  (18, 8, 3, 'Pending', NULL, NULL), -- TVs (variant_id 18) - 3 items
 
 -- Cart 9
-  (9, 9, 5, 'Pending', NULL), -- Mobile Phones (variant_id 9) - 5 items
+  (9, 9, 5, 'Pending', NULL, NULL), -- Mobile Phones (variant_id 9) - 5 items
 
 -- Cart 10 (Empty Cart)
 -- No items in the cart
 
 -- Cart 11 (Partial Cart)
-  (61, 11, 3, 'Pending', NULL), -- Home Appliances (variant_id 61) - 3 items
-  (71, 11, 2, 'Pending', NULL), -- Gaming Consoles (variant_id 71) - 2 items
+  (61, 11, 3, 'Pending', NULL, NULL), -- Home Appliances (variant_id 61) - 3 items
+  (71, 11, 2, 'Pending', NULL, NULL), -- Gaming Consoles (variant_id 71) - 2 items
 
 -- Cart 12
-  (10, 12, 4, 'Complete', '2023-10-15'),-- Mobile Phones (variant_id 10) - 4 items (sold)
+  (10, 12, 4, 'Complete', '2023-10-15', '649.99'),-- Mobile Phones (variant_id 10) - 4 items (sold)
 
 -- Cart 13 (Empty Cart)
 -- No items in the cart
 
 -- Cart 14
-  (70, 14, 1, 'Pending', NULL), -- Home Appliances (variant_id 70) - 1 item
+  (70, 14, 1, 'Pending', NULL, NULL), -- Home Appliances (variant_id 70) - 1 item
 
 -- Cart 15
-  (75, 15, 3, 'Pending', NULL), -- Gaming Consoles (variant_id 75) - 3 items
-  (85, 15, 2, 'Pending', NULL), -- Smart Home Security (variant_id 85) - 2 items
+  (75, 15, 3, 'Pending', NULL, NULL), -- Gaming Consoles (variant_id 75) - 3 items
+  (85, 15, 2, 'Pending', NULL, NULL), -- Smart Home Security (variant_id 85) - 2 items
 
 -- Cart 16
-  (80, 16, 4, 'Complete', '2023-10-16'), -- Gaming Consoles (variant_id 80) - 4 items (sold)
-  (90, 16, 2, 'Complete', '2023-10-16'), -- Smart Home Security (variant_id 90) - 2 items (sold)
+  (80, 16, 4, 'Complete', '2023-10-16', '549.99'), -- Gaming Consoles (variant_id 80) - 4 items (sold)
+  (90, 16, 2, 'Complete', '2023-10-16', '149.99'), -- Smart Home Security (variant_id 90) - 2 items (sold)
 
 -- Cart 17 (Empty Cart)
 -- No items in the cart
 
 -- Cart 18 (Partial Cart)
-  (40, 18, 2, 'Pending', NULL), -- Cameras (variant_id 40) - 2 items
+  (40, 18, 2, 'Pending', NULL, NULL), -- Cameras (variant_id 40) - 2 items
   
 -- Cart 19
-  (20, 19, 3, 'Complete', '2023-10-18'), -- Laptops (variant_id 20) - 3 items (sold)
-  (30, 19, 2, 'Complete', '2023-10-18'), -- Headphones (variant_id 30) - 2 items (sold)
+  (20, 19, 3, 'Complete', '2023-10-18', '449.99'), -- Laptops (variant_id 20) - 3 items (sold)
+  (30, 19, 2, 'Complete', '2023-10-18', '1199.99'), -- Headphones (variant_id 30) - 2 items (sold)
 
 -- Cart 20
-  (40, 20, 4, 'Pending', NULL), -- Cameras (variant_id 40) - 4 items
-  (50, 20, 3, 'Complete', '2023-10-19'), -- Watches (variant_id 50) - 3 items (sold)
+  (40, 20, 4, 'Pending', NULL, NULL), -- Cameras (variant_id 40) - 4 items
+  (50, 20, 3, 'Complete', '2023-10-19', '749.99'), -- Watches (variant_id 50) - 3 items (sold)
 
 -- Cart 21
-  (11, 21, 2, 'Complete', '2023-10-20'), -- Mobile Phones (variant_id 11) - 2 items (sold)
-  (21, 21, 1, 'Complete', '2023-10-20'), -- Laptops (variant_id 21) - 1 item (sold)
+  (11, 21, 2, 'Complete', '2023-10-20', '799.99'), -- Mobile Phones (variant_id 11) - 2 items (sold)
+  (21, 21, 1, 'Complete', '2023-10-20', '799.99'), -- Laptops (variant_id 21) - 1 item (sold)
 
 -- Cart 22 (Partial Cart)
-  (31, 22, 2, 'Pending', NULL), -- Headphones (variant_id 31) - 2 items
-  (41, 22, 1, 'Complete', '2023-10-21'), -- Cameras (variant_id 41) - 1 item (sold)
+  (31, 22, 2, 'Pending', NULL, NULL), -- Headphones (variant_id 31) - 2 items
+  (41, 22, 1, 'Complete', '2023-10-21', '799.99'), -- Cameras (variant_id 41) - 1 item (sold)
 
 -- Cart 23 (Empty Cart)
 -- No items in the cart
 
 -- Cart 24
-  (51, 24, 1, 'Pending', NULL), -- Watches (variant_id 51) - 1 item
+  (51, 24, 1, 'Pending', NULL, NULL), -- Watches (variant_id 51) - 1 item
 
 -- Cart 25
-  (60, 25, 2, 'Pending', NULL), -- Home Appliances (variant_id 60) - 2 items
-  (70, 25, 1, 'Complete', '2023-10-22'), -- Gaming Consoles (variant_id 70) - 1 item (sold)
+  (60, 25, 2, 'Pending', NULL, NULL), -- Home Appliances (variant_id 60) - 2 items
+  (70, 25, 1, 'Complete', '2023-10-22', '129.99'), -- Gaming Consoles (variant_id 70) - 1 item (sold)
 
 -- Cart 26 (Partial Cart)
-  (90, 26, 2, 'Pending', NULL), -- Smart Home Security (variant_id 90) - 2 items
+  (90, 26, 2, 'Pending', NULL, NULL), -- Smart Home Security (variant_id 90) - 2 items
 
 -- Cart 27
-  (10, 27, 3, 'Complete', '2023-10-23'), -- Mobile Phones (variant_id 10) - 3 items (sold)
-  (20, 27, 2, 'Pending', NULL), -- Laptops (variant_id 20) - 2 items
+  (10, 27, 3, 'Complete', '2023-10-23', '649.99'), -- Mobile Phones (variant_id 10) - 3 items (sold)
+  (20, 27, 2, 'Pending', NULL, NULL), -- Laptops (variant_id 20) - 2 items
 
 -- Cart 28
-  (30, 28, 4, 'Complete', '2023-10-24'), -- Headphones (variant_id 30) - 4 items (sold)
-  (40, 28, 2, 'Complete', '2023-10-24'), -- Cameras (variant_id 40) - 2 items (sold)
+  (30, 28, 4, 'Complete', '2023-10-24', '1199.99'), -- Headphones (variant_id 30) - 4 items (sold)
+  (40, 28, 2, 'Complete', '2023-10-24', '149.99'), -- Cameras (variant_id 40) - 2 items (sold)
 
 -- Cart 29 (Empty Cart)
 -- No items in the cart
 
 -- Cart 30
-  (50, 30, 3, 'Pending', NULL), -- Watches (variant_id 50) - 3 items
-  (70, 30, 2, 'Pending', NULL), -- Gaming Consoles (variant_id 70) - 2 items
+  (50, 30, 3, 'Pending', NULL, NULL), -- Watches (variant_id 50) - 3 items
+  (70, 30, 2, 'Pending', NULL, NULL), -- Gaming Consoles (variant_id 70) - 2 items
 
 -- Cart 31
-  (90, 31, 3, 'Pending', NULL), -- Smart Home Security (variant_id 90) - 3 items
-  (51, 31, 2, 'Complete', '2023-10-25'), -- Mobile Phones (variant_id 51) - 2 items (sold)
+  (90, 31, 3, 'Pending', NULL, NULL), -- Smart Home Security (variant_id 90) - 3 items
+  (51, 31, 2, 'Complete', '2023-10-25', '249.99'), -- Mobile Phones (variant_id 51) - 2 items (sold)
 
 -- Cart 32
-  (61, 32, 1, 'Pending', NULL), -- Home Appliances (variant_id 61) - 1 item
-  (71, 32, 3, 'Pending', NULL), -- Gaming Consoles (variant_id 71) - 3 items
+  (61, 32, 1, 'Pending', NULL, NULL), -- Home Appliances (variant_id 61) - 1 item
+  (71, 32, 3, 'Pending', NULL, NULL), -- Gaming Consoles (variant_id 71) - 3 items
 
 -- Cart 33
-  (31, 33, 4, 'Complete', '2023-10-26'), -- Headphones (variant_id 31) - 4 items (sold)
-  (91, 33, 2, 'Complete', '2023-10-26'), -- Printers (variant_id 91) - 2 items (sold)
+  (31, 33, 4, 'Complete', '2023-10-26', '199.99'), -- Headphones (variant_id 31) - 4 items (sold)
+  (91, 33, 2, 'Complete', '2023-10-26', '299.99'), -- Printers (variant_id 91) - 2 items (sold)
 
 -- Cart 34 (Partial Cart)
-  (102, 34, 1, 'Pending', NULL), -- Tablets (variant_id 102) - 1 item
-  (70, 34, 3, 'Complete', '2023-10-27'), -- Gaming Consoles (variant_id 70) - 3 items (sold)
+  (102, 34, 1, 'Pending', NULL, NULL), -- Tablets (variant_id 102) - 1 item
+  (70, 34, 3, 'Complete', '2023-10-27', '129.99'), -- Gaming Consoles (variant_id 70) - 3 items (sold)
 
 -- Cart 35 (Empty Cart)
 -- No items in the cart
 
 -- Cart 36
-  (40, 36, 4, 'Complete', '2023-10-28'), -- Cameras (variant_id 40) - 4 items (sold)
-  (51, 36, 2, 'Pending', NULL), -- Watches (variant_id 51) - 2 items
+  (40, 36, 4, 'Complete', '2023-10-28', '149.99'), -- Cameras (variant_id 40) - 4 items (sold)
+  (51, 36, 2, 'Pending', NULL, NULL), -- Watches (variant_id 51) - 2 items
 
 -- Cart 37
-  (61, 37, 1, 'Complete', '2023-10-29'), -- Home Appliances (variant_id 61) - 1 item (sold)
-  (70, 37, 3, 'Complete', '2023-10-29'), -- Gaming Consoles (variant_id 70) - 3 items (sold)
+  (61, 37, 1, 'Complete', '2023-10-29', '999.99'), -- Home Appliances (variant_id 61) - 1 item (sold)
+  (70, 37, 3, 'Complete', '2023-10-29', '129.99'), -- Gaming Consoles (variant_id 70) - 3 items (sold)
 
 -- Cart 38
-  (91, 38, 4, 'Pending', NULL), -- Printers (variant_id 91) - 4 items
-  (101, 38, 2, 'Pending', NULL), -- Tablets (variant_id 101) - 2 items
+  (91, 38, 4, 'Pending', NULL, NULL), -- Printers (variant_id 91) - 4 items
+  (101, 38, 2, 'Pending', NULL, NULL), -- Tablets (variant_id 101) - 2 items
 
 -- Cart 39 (Partial Cart)
-  (40, 39, 1, 'Complete', '2023-10-30'), -- Cameras (variant_id 40) - 1 item (sold)
-  (50, 39, 3, 'Pending', NULL), -- Watches (variant_id 50) - 3 items
+  (40, 39, 1, 'Complete', '2023-10-30', '149.99'), -- Cameras (variant_id 40) - 1 item (sold)
+  (50, 39, 3, 'Pending', NULL, NULL), -- Watches (variant_id 50) - 3 items
 
 -- Cart 40 (Empty Cart)
 -- No items in the cart
 
 -- Cart 41
-  (91, 41, 4, 'Pending', NULL), -- Printers (variant_id 91) - 4 items
-  (101, 41, 2, 'Pending', NULL); -- Tablets (variant_id 101) - 2 items
+  (91, 41, 4, 'Pending', NULL, NULL), -- Printers (variant_id 91) - 4 items
+  (101, 41, 2, 'Pending', NULL, NULL); -- Tablets (variant_id 101) - 2 items
 
 
 -- Dummy data for the Order table
