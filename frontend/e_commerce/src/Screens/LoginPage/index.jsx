cf0e6ad366ebb9d756deae08f92a25b5b3ed5612
Leshmith <122539516+Leshmith@users.noreply.github.com>
2023-10-19 10:17:53 +0530

    Update index.jsx

--- frontend/e_commerce/src/Screens/LoginPage/index.jsx
+++ frontend/e_commerce/src/Screens/LoginPage/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './index.css';
-
 import { Link } from 'react-router-dom';
-
 
 const LoginPage = () => {
     return (
         <div className='login-page'>
-            <input placeholder='username' type="text" />
-            <input placeholder='password' type="text" />
-            <button>Login</button>
-            <h1>This is the Login Page</h1>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/register"}>Register</Link>
+            <div className='login-form'>
+                <h1 className='login-heading'>Login</h1>
+                <input className='input-field' placeholder='Email' type="email" />
+                <input className='input-field' placeholder='Password' type="password" />
+                <button className='login-button'>Sign In</button>
+            </div>
+            <div className='login-links'>
+                <Link to="/">Home</Link>
+                <Link to="/register">Register</Link>
+            </div>
         </div>
     );
 };
