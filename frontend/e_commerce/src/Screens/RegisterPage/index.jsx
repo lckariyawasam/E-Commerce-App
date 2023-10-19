e3a6d3a41cf54938682d019ef6afbb0c3548ea23
Leshmith <122539516+Leshmith@users.noreply.github.com>
2023-10-19 10:19:11 +0530

    Update index.jsx

--- frontend/e_commerce/src/Screens/RegisterPage/index.jsx
+++ frontend/e_commerce/src/Screens/RegisterPage/index.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import './index.css';
-
 import { Link } from 'react-router-dom';
-
 
 const RegisterPage = () => {
     return (
         <div className='register-page'>
-            <input placeholder='name' type="text" />
-            <input placeholder='username' type="text" />
-            <input placeholder='password' type="text" />
-            <button>Register</button>
-            <h1>This is the Register Page</h1>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/login"}>Login</Link>
+            <div className='login-form'>
+                <h1 className='login-heading'>Register</h1>
+                <div className='name-fields'>
+                    <input className='input-field name-field ' placeholder='First Name' type="text" />
+                    <input className='input-field name-field' placeholder='Last Name' type="text" />
+                </div>
+                <input className='input-field' placeholder='Email' type="email" />
+                <input className='input-field' placeholder='Password' type="password" />
+                <input className='input-field' placeholder='Phone Number' type="tel" />
+                <div className='name-fields'>
+                    <input className='input-field name-field ' placeholder='Address Line 01' type="text" />
+                    <input className='input-field name-field' placeholder='City' type="text" />
+                </div>
+                <div className='name-fields'>
+                    <input className='input-field name-field ' placeholder='State' type="text" />
+                    <input className='input-field name-field' placeholder='Country' type="text" />
+                </div>
+                <button className='login-button'>Sign Up</button>
+            </div>
+            <div className='login-links'>
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+            </div>
         </div>
     );
 };
