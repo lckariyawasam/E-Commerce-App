10d6f68cc454226141fd6bab402855f1741a9291
Leshmith <122539516+Leshmith@users.noreply.github.com>
2023-10-19 10:39:49 +0530

    Update index.jsx

--- frontend/e_commerce/src/Screens/RegisterPage/index.jsx
+++ frontend/e_commerce/src/Screens/RegisterPage/index.jsx
@@ -8,19 +8,20 @@ const RegisterPage = () => {
             <div className='login-form'>
                 <h1 className='login-heading'>Register</h1>
                 <div className='name-fields'>
-                    <input className='input-field name-field ' placeholder='First Name' type="text" />
-                    <input className='input-field name-field' placeholder='Last Name' type="text" />
+                    <input className='input-field' placeholder='First Name' type="text" />
+                    <input className='input-field' placeholder='Last Name' type="text" />
                 </div>
                 <input className='input-field' placeholder='Email' type="email" />
                 <input className='input-field' placeholder='Password' type="password" />
                 <input className='input-field' placeholder='Phone Number' type="tel" />
+                <input className='input-field' placeholder='Address Line 01' type="text" />
                 <div className='name-fields'>
-                    <input className='input-field name-field ' placeholder='Address Line 01' type="text" />
-                    <input className='input-field name-field' placeholder='City' type="text" />
+                    <input className='input-field' placeholder='City' type="text" />
+                    <input className='input-field' placeholder='Zip-Code' type="text" />
                 </div>
                 <div className='name-fields'>
-                    <input className='input-field name-field ' placeholder='State' type="text" />
-                    <input className='input-field name-field' placeholder='Country' type="text" />
+                    <input className='input-field' placeholder='State/Province' type="text" />
+                    <input className='input-field' placeholder='Country' type="text" />
                 </div>
                 <button className='login-button'>Sign Up</button>
             </div>
