5e85e028fea9bd2f39c35eb68e0c1f4a7d5bbca1
Roshane Moraes <roshanem1772@gmail.com>
2023-11-01 21:47:28 +0530

    Update Footer and NavBar

--- frontend/e_commerce/src/Components/Navbar/index.jsx
+++ frontend/e_commerce/src/Components/Navbar/index.jsx
@@ -1,39 +1,76 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
-import SearchBar from '../SearchBar';
+const Navbar = ({ isLoggedIn, userName }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-custom sticky-top">
+      <a className="navbar-brand" href="/">
+        C-Enterprise
+      </a>
 
-const Navbar = ({ isLoggedIn }) => {
-   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-        <a className="navbar-brand" href="/">Your Brand</a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              <i className="fa fa-home"></i> Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/cart">
+              <i className="fa fa-shopping-cart"></i> Cart
+            </a>
+          </li>
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                    <a className="nav-link" href="/"><i className="fa fa-home"></i> Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/cart"><i className="fa fa-shopping-cart"></i> Cart</a>
-                </li>
-                {isLoggedIn ? (
-                    <li className="nav-item">
-                        <a className="nav-link" href="/profile"><i className="fa fa-user"></i> Profile</a>
-                    </li>
-                ) : (
-                    <li className="nav-item">
-                        <a className="nav-link" href="/login"><i className="fa fa-user"></i> Login/Register</a>
-                    </li>
-                )}
-            </ul>
-        <SearchBar isStrictSelection={false} endpoint={"products"}/>
-        </div>
+          {isLoggedIn ? (
+            <>
+              <li className="nav-item">
+                <a className="nav-link" href="/profile">
+                  <i className="fa fa-user"></i> Profile
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  <i className="fa fa-sign-out"></i> Sign Out
+                </a>
+              </li>
+              <li className="nav-item greeting">
+                <span className="nav-link">Hello, {userName}</span>
+              </li>
+            </>
+          ) : (
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                <i className="fa fa-user"></i> Login/Register
+              </a>
+            </li>
+          )}
+        </ul>
+        <form className="form-inline">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-light my-2 my-sm-0" type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
     </nav>
-   );
-}
+  );
+};
 
 export default Navbar;
