93bfca829350d4eb4f26e2b520d3b1d0615e72b6
Anjitha-Siyambalapitiya <130036355+Anjitha-Siyambalapitiya@users.noreply.github.com>
2023-11-01 15:59:09 +0530

    Update Combined_so_far.sql

--- Database/Combined_so_far.sql
+++ Database/Combined_so_far.sql
@@ -1250,6 +1250,30 @@ VALUES
   (43, 'Pending'),
   (44, 'Pending'),
   (45, 'Pending');
+
+--new carts for complete carts
+INSERT INTO Ecom_platform.Cart (user_id, status)
+VALUES
+  (1, 'Pending'),
+  (2, 'Pending'),  
+  (3, 'Pending'),
+  (4, 'Pending'),
+  (5, 'Pending'),
+  (6, 'Pending'),
+  (7, 'Pending'),  
+  (8, 'Pending'),
+  (9, 'Pending'),
+  (10, 'Pending'),
+  (11, 'Pending'), 
+  (12, 'Pending'),
+  (13, 'Pending'),    
+  (14, 'Pending'), 
+  (15, 'Pending'),
+  (16, 'Pending'),
+  (17, 'Pending'),
+  (18, 'Pending'),
+  (19, 'Pending'),   
+  (20, 'Pending');
   
 -- Example Cart_item records
 INSERT INTO Ecom_platform.Cart_item (variant_id, cart_id, quantity, status, sold_date, sold_price_per_item)
